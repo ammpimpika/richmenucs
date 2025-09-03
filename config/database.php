e7036ab45fb5f90fc2b14772335d082fe55bb462
ammpimpika <650112230043@bru.ac.th>
2025-09-03 15:50:58 +0700

    update

--- config/database.php
+++ config/database.php
@@ -1,10 +1,11 @@
 <?php
 function getConnection() {
-    $host = 'localhost';  
-    $dbname = 'chatbotcomsci'; 
-    $username = 'root';   
-    $password = '';       
-    $port = '3306';      
+    // ดึงค่าจาก Environment Variables
+    $host = getenv('DB_HOST');  
+    $dbname = getenv('DB_NAME'); 
+    $username = getenv('DB_USER');   
+    $password = getenv('DB_PASSWORD');       
+    $port = getenv('DB_PORT');      
 
     try {
         $pdo = new PDO(
